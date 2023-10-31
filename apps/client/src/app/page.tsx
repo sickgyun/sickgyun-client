b473eba39b203b5dfcddd0bdbdbdd150b182cb1c
SEOKKAMONI <sj01048260279@gmail.com>
2023-10-31 19:53:39 +0900

    feat(client): 로그인 박스, 배너 개발

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Header } from '@/components/common';
-import { Box, Text, Flex, Center } from '@chakra-ui/react';
+import { Box, Text, Flex, Center, Button, Image } from '@chakra-ui/react';
 
 const Home = () => {
   return (
@@ -9,28 +9,59 @@ const Home = () => {
       <Header />
       <Box width="100vw" height="100vh" backgroundColor="white">
         <Box margin="0 auto" paddingTop="30px" width="80%">
+          {/* 로그인 & 배너 섹션 */}
+          <Flex gap="36px" alignItems="center" marginBottom="48px">
+            <Image
+              src="/assets/mock_banner.jpeg"
+              objectFit="cover"
+              borderRadius="8px"
+              width="100%"
+              height="250px"
+              alt="Banner"
+            />
+            {/* 로그인 박스 */}
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              gap="48px"
+              padding="32px"
+              borderRadius="8px"
+              border="1px solid"
+              borderColor="gray.200"
+              width="500px"
+              height="250px"
+            >
+              <div>
+                <Text as="p" fontSize="20px">
+                  로그인하고
+                </Text>
+                <Text as="p" fontSize="20px">
+                  다양한 정보를 얻어보세요.
+                </Text>
+              </div>
+              <Button width="100%">로그인</Button>
+            </Box>
+          </Flex>
           {/* 직군별 리스트 */}
-          <Box display="flex" justifyContent="center">
-            <Flex gap="16px">
-              {Array(5)
-                .fill('')
-                .map((_, i) => (
-                  <Center
-                    transition="all 0.25s ease"
-                    borderRadius="8px"
-                    width="200px"
-                    height="120px"
-                    _hover={{
-                      backgroundColor: 'gray.50',
-                      cursor: 'pointer',
-                      margin: '0 auto',
-                    }}
-                  >
-                    Frontend
-                  </Center>
-                ))}
-            </Flex>
-          </Box>
+          <Flex justifyContent="space-between">
+            {Array(5)
+              .fill('')
+              .map((_, i) => (
+                <Center
+                  transition="all 0.25s ease"
+                  borderRadius="8px"
+                  width="200px"
+                  height="120px"
+                  _hover={{
+                    backgroundColor: 'gray.50',
+                    cursor: 'pointer',
+                  }}
+                >
+                  Frontend
+                </Center>
+              ))}
+          </Flex>
         </Box>
       </Box>
     </>
