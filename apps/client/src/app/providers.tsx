27e09e81731262c6cfc16e15fa0d9a6df034bd0a
SEOKKAMONI <sj01048260279@gmail.com>
2023-10-31 16:26:06 +0900

    feat(client): providers 추가

---
+++ apps/client/src/app/providers.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import React from 'react';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+export const theme = extendTheme({ colors });
+
+type ProvidersProps = {
+  children: React.ReactNode;
+};
+
+export const Providers = ({ children }: ProvidersProps) => {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+};
