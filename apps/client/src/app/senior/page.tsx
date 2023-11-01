c05cd41473c471f61c6d7f1af7fffc6b75941711
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-01 22:45:22 +0900

    chore(client): 선배 정보 카드 깔끔하게 수정

--- apps/client/src/app/senior/page.tsx
+++ apps/client/src/app/senior/page.tsx
@@ -33,14 +33,14 @@ const SeniorPage = () => {
           />
           <Box display="flex" alignItems="center" gap="16px" marginBottom="48px">
             {POSITION_LIST.map((position) => (
-                <Link
-                  onClick={() => setSelectedPosition(position)}
-                  color={selectedPosition === position ? 'primary' : 'gray.700'}
-                  _hover={{ color: 'primary' }}
-                >
-                  {position}
-                </Link>
-              ))}
+              <Link
+                onClick={() => setSelectedPosition(position)}
+                color={selectedPosition === position ? 'primary' : 'gray.700'}
+                _hover={{ color: 'primary' }}
+              >
+                {position}
+              </Link>
+            ))}
           </Box>
           <Grid templateColumns="repeat(2, 1fr)" gap="32px" marginBottom="64px">
             {Array(10)
@@ -49,9 +49,6 @@ const SeniorPage = () => {
                 // 선배 카드
                 <Box
                   transition="all 0.25s ease"
-                  display="flex"
-                  alignItems="flex-start"
-                  gap="24px"
                   padding="24px"
                   border="1px solid"
                   borderColor="gray.200"
@@ -62,42 +59,46 @@ const SeniorPage = () => {
                   }}
                   height="144px"
                 >
-                  <Image
-                    src="/assets/mock_senior.jpeg"
-                    borderRadius="8px"
-                    width="100px"
-                    height="100px"
-                    alt="Senior"
-                  />
-                  <Flex flexDirection="column" gap="10px">
-                    <div>
-                      <Flex alignItems="center" gap="6px">
-                        <Text fontSize="24px" fontWeight="semibold">
-                          김석진
-                        </Text>
-                        <Text fontSize="16px" color="gray.900" fontWeight="medium">
-                          2기 - 토스페이먼츠
-                        </Text>
+                  <Flex gap="24px" height="100%">
+                    <Image
+                      src="/assets/mock_senior.jpeg"
+                      borderRadius="8px"
+                      width="95px"
+                      height="100%"
+                      alt="Senior"
+                    />
+                    <Flex flexDirection="column" justifyContent="center" height="100%">
+                      <Flex flexDirection="column" gap="6px">
+                        <Flex flexDirection="column">
+                          <Flex alignItems="center" gap="6px">
+                            <Text fontSize="24px" fontWeight="semibold">
+                              김석진
+                            </Text>
+                            <Text fontSize="16px" color="gray.900" fontWeight="medium">
+                              2기 - 토스페이먼츠
+                            </Text>
+                          </Flex>
+                          <Text maxWidth="100%" color="gray.600" fontSize="14px">
+                            즐거움을 토대로 토대를 만드는 도리"토스"를 좋아하는 개발자
+                          </Text>
+                        </Flex>
+                        <Box
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="center"
+                          color="gray.700"
+                          border="1px solid"
+                          borderColor="gray.200"
+                          borderRadius="18px"
+                          fontSize="14px"
+                          fontWeight="medium"
+                          width="80px"
+                          height="24px"
+                        >
+                          프론트엔드
+                        </Box>
                       </Flex>
-                      <Text maxWidth="100%" color="gray.600" fontSize="14px">
-                        즐거움을 토대로 토대를 만드는 도리"토스"를 좋아하는 개발자
-                      </Text>
-                    </div>
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      color="gray.700"
-                      border="1px solid"
-                      borderColor="gray.200"
-                      borderRadius="18px"
-                      fontSize="14px"
-                      fontWeight="medium"
-                      width="80px"
-                      height="24px"
-                    >
-                      프론트엔드
-                    </Box>
+                    </Flex>
                   </Flex>
                 </Box>
               ))}
