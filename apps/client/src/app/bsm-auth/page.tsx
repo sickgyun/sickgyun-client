41df5aeefd59e585c5d80517324883b574c94e60
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-07 11:21:59 +0900

    refactor(client): 로그인 성공, 실패 외부에서 처리

--- apps/client/src/app/bsm-auth/page.tsx
+++ apps/client/src/app/bsm-auth/page.tsx
@@ -1,21 +1,49 @@
 'use client';
 
 import { Box, Spinner } from '@chakra-ui/react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+import { LOCAL_STORAGE_KEY } from '@/constants/storage';
 import { useLoginBsmMutation } from '@/hooks/api/auth/useLoginBsmMutation';
 
 const BsmAuth = () => {
+  const router = useRouter();
   const params = useSearchParams();
 
-  const { mutate: loginBsmMutate } = useLoginBsmMutation();
+  const {
+    mutate: loginBsmMutate,
+    data: loginBsmData,
+    error: loginBsmError,
+  } = useLoginBsmMutation();
 
   useEffect(() => {
     const authCode = params.get('code');
+
     if (authCode) {
       loginBsmMutate({ authCode });
     }
   }, [loginBsmMutate, params]);
+
+  useEffect(() => {
+    if (loginBsmData) {
+      if (loginBsmData.data) {
+        localStorage.setItem(
+          LOCAL_STORAGE_KEY.accessToken,
+          loginBsmData.data.accessToken
+        );
+        router.replace('/');
+      } else {
+        alert('로그인 도중 오류가 발생하였습니다. 다시 한번 시도해주세요.');
+      }
+    }
+  }, [loginBsmData, router]);
+
+  useEffect(() => {
+    if (loginBsmError) {
+      alert('로그인 도중 오류가 발생하였습니다. 다시 한번 시도해주세요.');
+      router.replace('/');
+    }
+  }, [loginBsmError, router]);
 
   return (
     <Box
--- apps/client/src/hooks/api/auth/useLoginBsmMutation.ts
+++ apps/client/src/hooks/api/auth/useLoginBsmMutation.ts
@@ -1,6 +1,4 @@
 import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import { LOCAL_STORAGE_KEY } from '@/constants/storage';
 import { post } from '@/libs/api/client';
 
 type LoginBsmMutationResponse = {
@@ -15,8 +13,6 @@ type LoginBsmMutationParams = {
 };
 
 export const useLoginBsmMutation = () => {
-  const router = useRouter();
-
   return useMutation<
     LoginBsmMutationResponse,
     { message?: string },
@@ -24,13 +20,5 @@ export const useLoginBsmMutation = () => {
   >({
     mutationFn: ({ authCode }: LoginBsmMutationParams) =>
       post<LoginBsmMutationResponse>(`/auth?code=${authCode}`),
-    onSuccess: (data: LoginBsmMutationResponse) => {
-      localStorage.setItem(LOCAL_STORAGE_KEY.accessToken, data.data.accessToken);
-      router.replace('/');
-    },
-    onError: () => {
-      alert('로그인 도중 오류가 발생하였습니다. 다시 한번 시도해주세요.');
-      router.replace('/');
-    },
   });
 };
