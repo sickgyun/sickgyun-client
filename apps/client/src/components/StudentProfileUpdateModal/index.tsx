cda9efa0b2d5aaa1dd703324e3308c2f9ce597e4
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-17 01:55:05 +0900

    feat(client): 내 정보 불러오기

--- apps/client/src/components/StudentProfileUpdateModal/index.tsx
+++ apps/client/src/components/StudentProfileUpdateModal/index.tsx
@@ -14,6 +14,8 @@ import {
 } from '@chakra-ui/react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
+import { useGetStudentProfile } from '@/hooks/api/student-profile/useGetStudentProfile';
+import { useUserInformation } from '@/store/UserInformation';
 
 type StudentProfileUpdateFormInput = {
   githubId?: string;
@@ -29,6 +31,7 @@ const StudentProfileUpdateModal = ({
   isOpen,
   onClose,
 }: StudentProfileUpdateModalProps) => {
+  const { userInformation } = useUserInformation();
   const { register, handleSubmit: handleUpdateStudentProfileSubmit } =
     useForm<StudentProfileUpdateFormInput>();
 
@@ -38,6 +41,8 @@ const StudentProfileUpdateModal = ({
     console.log(data);
     onClose();
   };
+
+  const { studentProfile } = useGetStudentProfile(userInformation.userCode);
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="lg">
@@ -54,22 +59,26 @@ const StudentProfileUpdateModal = ({
           <Flex flexDirection="column" gap="16px">
             <Flex gap="8px">
               <Input
-                value={userInformation.name}
+                value={studentProfile.name}
                 placeholder="이름을 입력해주세요."
                 disabled
               />
               <Input
-                defaultValue={userInformation.githubId}
+                defaultValue={studentProfile.githubId}
                 placeholder="깃허브 아이디를 입력해주세요."
                 {...register('githubId')}
               />
             </Flex>
             <Input
-              defaultValue={userInformation.email}
+              defaultValue={studentProfile.email}
               placeholder="이메일을 적어주세요."
               {...register('email')}
             />
-            <Input placeholder="소개 말을 적어주세요." {...register('bio')} />
+            <Input
+              defaultValue={studentProfile.bio}
+              placeholder="소개 말을 적어주세요."
+              {...register('bio')}
+            />
             <Select size="md" {...register('position')}>
               <option value="FRONTEND">프론트엔드</option>
               <option value="BACKEND">백엔드</option>
@@ -77,16 +86,18 @@ const StudentProfileUpdateModal = ({
               <option value="APP">앱</option>
               <option value="DESIGNER">디자이너</option>
             </Select>
-            {userInformation.isGraduate && (
+            {studentProfile.isGraduate && (
               <Input placeholder="회사명을 입력해주세요." {...register('company')} />
             )}
           </Flex>
         </ModalBody>
         <ModalFooter>
-          <Button type="submit" color="red.700" variant="outline">
-            삭제
-          </Button>
-          <Button type="submit">등록</Button>
+          <Flex gap="12px">
+            <Button type="submit" colorScheme="red" variant="outline">
+              내 프로필 삭제
+            </Button>
+            <Button type="submit">수정</Button>
+          </Flex>
         </ModalFooter>
       </ModalContent>
     </Modal>
