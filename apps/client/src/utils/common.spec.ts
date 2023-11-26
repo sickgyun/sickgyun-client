9d7ed02b235fb4a44f2f85673342fbf13b541b84
SEOKKAMONI <sj01048260279@gmail.com>
2023-11-26 23:44:35 +0900

    test(client): getUserProfileImage 테스트 코드 작성

--- apps/client/src/utils/common.spec.ts
+++ apps/client/src/utils/common.spec.ts
@@ -1,7 +1,7 @@
 import { isProd } from './common';
 
 describe('isProd', () => {
-  it(`인자가 'production'이면 반환 값은 'true'이다.`, () => {
+  it(`인자가 'production'이면 반환 값은 'true'를 반환해야 합니다.`, () => {
     const env = 'production';
 
     const result = isProd(env);
@@ -9,7 +9,7 @@ describe('isProd', () => {
     expect(result).toBe(true);
   });
 
-  it(`인자가 'production'이 아니면 반환 값은 'false'이다.`, () => {
+  it(`인자가 'production'이 아니라면 반환 값은 'false'를 반환해야 합니다.`, () => {
     const env = 'development';
 
     const result = isProd(env);
