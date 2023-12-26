7382ddcf2bea33f605188309cc8ce22f6a2ce512
김석진 <102217654+SEOKKAMONI@users.noreply.github.com>
2023-12-26 15:29:35 +0900

    feat(ui): ui package 세팅 (#92)
    * feat(ui): ui package 세팅
    
    * feat(ui): storybook 세팅
    
    * chore(design-token): type 인식 에러 수정

---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  root: true,
+  extends: ['@sickgyun/eslint-config/react-ts', 'plugin:storybook/recommended'],
+  ignorePatterns: ['*.js*', 'dist'],
+};
---
+++ packages/ui/.storybook/preview.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { theme, GlobalStyle } from '@sickgyun/design-token';
+
+export const preview = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
