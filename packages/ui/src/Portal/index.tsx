bdff63daa23579ff61945c4ec74f715583569512
김석진 <102217654+SEOKKAMONI@users.noreply.github.com>
2023-12-26 21:56:13 +0900

    feat(ui): portal 컴포넌트 개발 (#100)
    * feat(ui): portal 컴포넌트 개발
    
    * feat(common): pnpm-lock.yaml 업데이트

---
+++ packages/ui/src/Portal/index.tsx
@@ -0,0 +1,21 @@
+import { type PropsWithChildren, useState } from 'react';
+import { createPortal } from 'react-dom';
+import useDidMount from './useDidMount';
+
+type PortalProps = PropsWithChildren;
+
+export const Portal = ({ children }: PortalProps) => {
+  const [container, setContainer] = useState<Element | null>(null);
+
+  useDidMount(() => {
+    if (document) {
+      setContainer(document.body);
+    }
+  });
+
+  if (!container) return null;
+
+  return createPortal(children, container);
+};
+
+export default Portal;
