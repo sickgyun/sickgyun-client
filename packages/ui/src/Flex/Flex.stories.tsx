486e6735d88cf70320922901fd0fdb145d4e48c9
김석진 <102217654+SEOKKAMONI@users.noreply.github.com>
2023-12-26 21:56:37 +0900

    feat(ui): flex 컴포넌트 개발 (#98)
    * feat(ui): flex 컴포넌트 개발
    
    * feat(ui): flex storybook 작성

---
+++ packages/ui/src/Flex/Flex.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Flex as FlexComponent } from '.';
+
+type Flex = typeof FlexComponent;
+
+const meta: Meta<Flex> = {
+  argTypes: {
+    direction: {
+      control: 'select',
+      options: ['row', 'row-reverse', 'column', 'column-reverse'],
+    },
+    align: {
+      control: 'select',
+      options: ['flex-start', 'flex-end', 'center', 'baseline', 'stretch'],
+    },
+    justify: {
+      control: 'select',
+      options: ['flex-start', 'flex-end', 'center', 'space-between', 'space-around'],
+    },
+    wrap: {
+      control: 'select',
+      options: ['nowrap', 'wrap', 'wrap-reverse'],
+    },
+    basis: {
+      control: 'text',
+    },
+    grow: {
+      control: 'number',
+    },
+    shrink: {
+      control: 'number',
+    },
+  },
+  component: FlexComponent,
+  title: 'Components/Flex',
+};
+
+export default meta;
+
+export const Default: StoryObj<Flex> = {
+  render: (args) => (
+    <FlexComponent {...args}>
+      <div style={{ width: '80px', height: '80px', backgroundColor: 'red' }} />
+      <div style={{ width: '80px', height: '80px', backgroundColor: 'green' }} />
+      <div style={{ width: '80px', height: '80px', backgroundColor: 'blue' }} />
+    </FlexComponent>
+  ),
+};
