8c7f732102e0ab15f5577fad966e18055fbeff4a
김석진 <sj01048260279@gmail.com>
2023-12-26 14:34:09 +0900

    feat(ui): storybook 세팅

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: true,
-  extends: ['@sickgyun/eslint-config/react-ts'],
+  extends: ['@sickgyun/eslint-config/react-ts', 'plugin:storybook/recommended'],
   ignorePatterns: ['*.js*', 'dist'],
 };
