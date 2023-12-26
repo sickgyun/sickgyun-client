7a15a9ad1fe9497a4f85f64c56d16674978ffe49
김석진 <102217654+SEOKKAMONI@users.noreply.github.com>
2023-12-26 16:00:15 +0900

    feat(ui): text 컴포넌트 개발 (#94)
    * feat(ui): text 컴포넌트 개발
    
    * feat(ui): text storybook 작성
    
    * feat(design-token): font 적용

--- packages/design-token/src/font.ts
+++ packages/design-token/src/font.ts
@@ -3,6 +3,6 @@ import { css } from '@emotion/react';
 export const FontCSS = css`
   @font-face {
     font-family: 'WantedSansVariable';
-    src: url('/fonts/WantedSansVariable.woff2') format('woff2');
+    src: url('./fonts/WantedSansVariable.woff2') format('woff2');
   }
 `;
