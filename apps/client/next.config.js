f46d66ad160dffddfde4ec36bfc2ce2285cc08c4
김석진 <sj01048260279@gmail.com>
2023-12-27 13:54:36 +0900

    refactor(client): job-posting 컴포넌트 chakra ui 삭제

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@sickgyun/design-token', '@sickgyun/libs', '@sickgyun/utils', '@sickgyun/ui'],
+  images: {
+    domains: ['cdn.rallit.com'], 
+  },
 };
 
 module.exports = nextConfig;
--- apps/client/src/components/job-posting/JobPostingList/index.tsx
+++ apps/client/src/components/job-posting/JobPostingList/index.tsx
@@ -1,20 +1,26 @@
-import { Grid } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import JobPostingCard from '../JobPostingCard';
 import { JOB_POSTING } from '@/constants/job-posting';
 
 const JobPostingList = () => {
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap="32px">
+    <StyledJobPostingList>
       {JOB_POSTING.map((jobPosting) => (
         <JobPostingCard
           title={jobPosting.title}
-          imageUrl={jobPosting.imageUrl}
+          thumbnail={jobPosting.thumbnail}
           companyName={jobPosting.companyName}
           detailLink={jobPosting.detailLink}
         />
       ))}
-    </Grid>
+    </StyledJobPostingList>
   );
 };
 
 export default JobPostingList;
+
+const StyledJobPostingList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 32px;
+`;
