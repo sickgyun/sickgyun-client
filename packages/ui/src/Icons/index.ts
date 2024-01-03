26954d6bf951f4dee66f4cbbdb2403b3612816c3
김석진 <sj01048260279@gmail.com>
2024-01-03 23:55:19 +0900

    feat(ui): select 다양한 케이스에 대한 대응

--- packages/ui/src/Icons/index.ts
+++ packages/ui/src/Icons/index.ts
@@ -1,3 +1,4 @@
 export * from './Close';
 export * from './Add';
 export * from './ArrowBack';
+export * from './ArrowDown';
--- packages/ui/src/Select/index.tsx
+++ packages/ui/src/Select/index.tsx
@@ -2,6 +2,8 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { ForwardedRef, ReactNode, SelectHTMLAttributes } from 'react';
 import { forwardRef } from 'react';
+import { Flex } from '../Flex';
+import { ArrowDownIcon } from '../Icons';
 import { Text } from '../Text';
 
 type SelectProps = {
@@ -11,19 +13,28 @@ type SelectProps = {
 } & SelectHTMLAttributes<HTMLSelectElement>;
 
 export const Select = forwardRef(function Select(
-  { children, label, width = '100%', ...props }: SelectProps,
+  { children, placeholder, label, width = '100%', ...props }: SelectProps,
   ref: ForwardedRef<HTMLSelectElement>
 ) {
   return (
     <StyledSelectWrapper width={width}>
       {label && (
-        <Text color="gray600" fontType="p3">
+        <Text color="gray600" fontType="p3" style={{ marginBottom: '8px' }}>
           {label}
         </Text>
       )}
-      <StyledSelect ref={ref} {...props}>
-        {children}
-      </StyledSelect>
+      <Flex align="center" style={{ position: 'relative' }}>
+        <StyledSelect ref={ref} {...props}>
+          {placeholder && <option value="">{placeholder}</option>}
+          {children}
+        </StyledSelect>
+        <ArrowDownIcon
+          width={24}
+          height={24}
+          cursor="pointer"
+          style={{ position: 'absolute', right: '16px' }}
+        />
+      </Flex>
     </StyledSelectWrapper>
   );
 });
@@ -40,18 +51,21 @@ const StyledSelectWrapper = styled.div<{ width: string }>`
 const StyledSelect = styled.select`
   width: 100%;
   height: 48px;
+  border-radius: 16px;
   padding-left: 16px;
   padding-right: 16px;
-  border-radius: 16px;
   border: none;
   outline: none;
-  ${({ theme }) => css`
-    color: ${theme.colors.black};
-    border: 1.5px solid ${theme.colors.gray400};
-    background-color: ${theme.colors.white};
-    ::placeholder {
-      ${theme.fonts.body1}
-      color: ${theme.colors.gray500};
-    }
-  `}
+  -webkit-appearance: none; /* 크롬 화살표 없애기 */
+  -moz-appearance: none; /* 파이어폭스 화살표 없애기 */
+  appearance: none /* 화살표 없애기 */
+    ${({ theme }) => css`
+      color: ${theme.colors.black};
+      border: 1.5px solid ${theme.colors.gray400};
+      background-color: ${theme.colors.white};
+      ::placeholder {
+        ${theme.fonts.body1}
+        color: ${theme.colors.gray500};
+      }
+    `};
 `;
