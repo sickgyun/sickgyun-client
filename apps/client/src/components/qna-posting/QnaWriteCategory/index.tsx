74a133df03d4edb6a541487f6e0490ba3543649f
lsj0202 <w1004123@gmail.com>
2024-01-19 15:44:00 +0900

    feat(client): qna write page category bar 생성

---
+++ apps/client/src/components/qna-posting/QnaWriteCategory/index.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import { Stack, Text } from '@sickgyun/ui';
+import { useState } from 'react';
+import QnaCategory from '../QnaCategory';
+import { QNA_WRITE_CATEGORY } from '@/constants/qna-write';
+
+const QnaWriteCategory = () => {
+  const [activeCategoryIndex, setActiveCategoryIndex] = useState(0);
+
+  return (
+    <StyledQnaWriteCategory>
+      <Text fontType="h4" style={{ fontWeight: 'bold' }}>
+        카테고리
+      </Text>
+      <Stack direction="horizontal" spacing={10}>
+        {QNA_WRITE_CATEGORY.map((category) => (
+          <QnaCategory
+            questionType={category.title}
+            isWriteCategory
+            isActive={activeCategoryIndex === category.id}
+            onClick={() => setActiveCategoryIndex(category.id)}
+          />
+        ))}
+      </Stack>
+    </StyledQnaWriteCategory>
+  );
+};
+
+export default QnaWriteCategory;
+
+const StyledQnaWriteCategory = styled.div`
+  width: 100%;
+  height: 115px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.white};
+  padding: 22px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
---
+++ apps/client/src/constants/qna-write.ts
@@ -0,0 +1,17 @@
+export const QNA_WRITE_CATEGORY = [
+  {
+    id: 0,
+    title: '개발',
+    emoji: '💻',
+  },
+  {
+    id: 1,
+    title: '취업',
+    emoji: '👔',
+  },
+  {
+    id: 2,
+    title: '고민',
+    emoji: '🤔',
+  },
+];
