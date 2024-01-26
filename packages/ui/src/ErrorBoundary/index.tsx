8ad8b662fef8b7ce68b17e71478843168ebd3d98
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-01-27 03:15:04 +0900

    feat(ui): error boundary 컴포넌트 개발 (#163)

---
+++ packages/ui/src/ErrorBoundary/index.tsx
@@ -0,0 +1,44 @@
+import type { ErrorInfo, ReactNode } from 'react';
+import { Component } from 'react';
+
+type ErrorBoundaryState = {
+  error?: Error;
+};
+
+type ErrorBoundaryProps = {
+  fallback?: ReactNode | ((args: any) => ReactNode);
+  children?: ReactNode;
+};
+
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { error: undefined };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
+      error,
+    });
+
+    console.error(errorInfo);
+  }
+
+  render() {
+    const { fallback, children } = this.props;
+
+    if (this.state.error) {
+      if (!fallback) {
+        return null;
+      }
+
+      if (typeof fallback === 'function') {
+        return fallback({ error: this.state.error });
+      }
+
+      return fallback;
+    }
+
+    return children;
+  }
+}
