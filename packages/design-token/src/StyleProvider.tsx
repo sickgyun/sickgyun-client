38a4051ad39eab64946ea73b695c65b7ae00ce6f
SEOKKAMONI <sj01048260279@gmail.com>
2024-01-27 21:16:20 +0900

    feat(design-token): theme provider ssr 대응

--- packages/design-token/src/StyleProvider.tsx
+++ packages/design-token/src/StyleProvider.tsx
@@ -1,14 +1,43 @@
-import type { ThemeProviderProps } from '@emotion/react';
-import { ThemeProvider } from '@emotion/react';
-import { GlobalStyle, theme } from '..';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import type { EmotionCache, Theme, ThemeProviderProps } from '@emotion/react';
+import { GlobalStyle } from './global';
+import { theme as sickgyunTheme } from './themes';
 
-type StyleProviderProps = Omit<ThemeProviderProps, 'theme'>;
+type StyleProviderProps = {
+  theme?: Partial<Theme> | ((outerTheme: Theme) => Theme);
+} & Omit<ThemeProviderProps, 'theme'>;
+type UseSSRProps = {
+  isSSR: true;
+  cache: EmotionCache;
+} & StyleProviderProps;
+type UseCSRProps = {
+  isSSR?: false;
+  cache?: never;
+} & StyleProviderProps;
 
-export const StyleProvider = ({ children }: StyleProviderProps) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
-  );
+type SickgyunStyleProviderProps = UseSSRProps | UseCSRProps;
+
+export const StyleProvider = ({
+  isSSR = false,
+  cache,
+  children,
+  theme = sickgyunTheme,
+}: SickgyunStyleProviderProps) => {
+  if (isSSR && cache) {
+    return (
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </CacheProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    );
+  }
 };
