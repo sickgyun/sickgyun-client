347a5e1450878719fb2aab326732119736fbd2dd
SEOKKAMONI <sj01048260279@gmail.com>
2024-01-27 17:08:15 +0900

    feat(common): zIndex design-token 추가

--- packages/design-token/src/types/emotion.d.ts
+++ packages/design-token/src/types/emotion.d.ts
@@ -1,9 +1,10 @@
 import '@emotion/react';
-import type { Colors, Fonts } from '..';
+import type { Colors, Fonts, ZIndex } from '..';
 
 declare module '@emotion/react' {
   export interface Theme {
     colors: Colors;
     fonts: Fonts;
+    zIndex: ZIndex;
   }
 }
