fd35206563efd296e3761ebbd16e88c8dc22d886
이상진 <102288397+lsj0202@users.noreply.github.com>
2024-01-27 20:40:19 +0900

    feat(ui): spinner 컴포넌트 개발 (#171)
    * feat(ui): spinner 컴포넌트 개발
    
    * feat(ui): spinner storybook 작성
    
    * chore(ui): lint error 수정
    
    * chore(ui): 리뷰 반영
    
    * chore(ui): 리뷰 반영

---
+++ packages/ui/src/Spinner/index.tsx
@@ -0,0 +1,44 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+import { colors } from '@sickgyun/design-token';
+import { type ForwardedRef, forwardRef } from 'react';
+
+type SpinnerProps = {
+  size?: number;
+  color?: string;
+};
+
+export const Spinner = forwardRef(function Spinner(
+  { size = 50, color = colors.primary, ...props }: SpinnerProps,
+  ref: ForwardedRef<HTMLDivElement>
+) {
+  return (
+    <StyledSpinnerWrapper>
+      <StyledSpinner ref={ref} color={color} size={size} {...props} />
+    </StyledSpinnerWrapper>
+  );
+});
+
+const StyledSpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledSpinner = styled.div<SpinnerProps>`
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+  border: 9px solid ${colors.gray100};
+  border-top: 9px solid ${({ color }) => color};
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+`;
