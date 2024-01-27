2df1c4848a060753b19048081a119904049ad746
SEOKKAMONI <sj01048260279@gmail.com>
2024-01-28 03:27:33 +0900

    feat(common): chakra-ui 삭제

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -3,16 +3,14 @@
 import { StyleProvider } from '@sickgyun/design-token';
 import { OverlayProvider } from '@toss/use-overlay';
 import type { PropsWithChildren } from 'react';
-import { ChakraProvider, QueryClientProvider } from '@/providers';
+import { QueryClientProvider } from '@/providers';
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
     <QueryClientProvider>
-      <ChakraProvider>
-        <OverlayProvider>
-          <StyleProvider>{children}</StyleProvider>
-        </OverlayProvider>
-      </ChakraProvider>
+      <OverlayProvider>
+        <StyleProvider>{children}</StyleProvider>
+      </OverlayProvider>
     </QueryClientProvider>
   );
 };
