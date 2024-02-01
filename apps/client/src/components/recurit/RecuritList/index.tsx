6773ee036992c91db347824ea60fc6c4f2f3078b
SEOKKAMONI <sj01048260279@gmail.com>
2024-02-01 23:15:43 +0900

    feat(client): 채용 정보 조회 기능 개발

--- apps/client/src/components/recurit/RecuritList/index.tsx
+++ apps/client/src/components/recurit/RecuritList/index.tsx
@@ -1,26 +1,49 @@
 import styled from '@emotion/styled';
 import { Stack, Text } from '@sickgyun/ui';
 import Image from 'next/image';
+import { withSuspense } from '@/hocs/withSuspense';
+import { useGetRecuritList } from '@/hooks/api/recruit/useGetRecuritList';
 
-type JobPostingCardProps = {
+const RecuritList = () => {
+  const { recuritList } = useGetRecuritList();
+
+  return (
+    <StyledRecuritList>
+      {recuritList?.map((recurit) => (
+        <RecuritCard
+          key={recurit.id}
+          title={recurit.reqruitName}
+          thumbnail={recurit.imageSrc}
+          companyName={recurit.company}
+          detailLink=""
+        />
+      ))}
+    </StyledRecuritList>
+  );
+};
+
+export default withSuspense(RecuritList);
+
+const StyledRecuritList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 32px;
+`;
+
+type RecuritCardProps = {
   title: string;
   thumbnail?: string;
   companyName: string;
   detailLink: string;
 };
 
-const JobPostingCard = ({
-  title,
-  thumbnail,
-  companyName,
-  detailLink,
-}: JobPostingCardProps) => {
+const RecuritCard = ({ title, thumbnail, companyName, detailLink }: RecuritCardProps) => {
   const handleGoComapnyDetailPage = () => {
     window.open(detailLink);
   };
 
   return (
-    <StyledJobPostingCard onClick={handleGoComapnyDetailPage}>
+    <StyledRecuritCard onClick={handleGoComapnyDetailPage}>
       <StyledThumnailImage src={thumbnail} width={85} height={85} alt="Job Posting" />
       <Stack spacing={4}>
         <Text fontType="h4">{title}</Text>
@@ -28,13 +51,11 @@ const JobPostingCard = ({
           {companyName}
         </Text>
       </Stack>
-    </StyledJobPostingCard>
+    </StyledRecuritCard>
   );
 };
 
-export default JobPostingCard;
-
-const StyledJobPostingCard = styled.div`
+const StyledRecuritCard = styled.div`
   display: flex;
   align-items: center;
   gap: 24px;
---
+++ apps/client/src/hooks/api/recruit/useGetRecuritList.ts
@@ -0,0 +1,26 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { get } from '@/libs/api/client';
+
+type RecuritListResponse = {
+  id: number;
+  company: string;
+  imageSrc: string;
+  reqruitName: string;
+  skills: string[];
+};
+
+export const RECURIT_LIST_QUERY_KEY = 'recuritList';
+
+export const useGetRecuritList = (size = 6) => {
+  const recuritListQuery = useSuspenseQuery<RecuritListResponse[]>({
+    queryKey: [RECURIT_LIST_QUERY_KEY],
+    queryFn: async () => {
+      return await get<RecuritListResponse[]>(`/api/reqruit?size=${size}`);
+    },
+  });
+
+  return {
+    recuritList: recuritListQuery.data,
+    ...recuritListQuery,
+  };
+};
