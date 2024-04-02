8d9d52e3064753f283f19c197e4eae40877eeae2
김석진 <sj01048260279@gmail.com>
2024-04-02 19:37:13 +0900

    feat(client): 답장이 완료된 커피챗 요청 카드 디자인 추가

--- apps/client/src/components/coffeechat/CoffeechatReceiveCard/index.tsx
+++ apps/client/src/components/coffeechat/CoffeechatReceiveCard/index.tsx
@@ -1,21 +1,25 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { Button, Stack, Text } from '@sickgyun/ui';
+import { Button, Flex, Stack, Text } from '@sickgyun/ui';
 import { useOverlay } from '@toss/use-overlay';
 import CoffeechatAcceptConfirm from '../CoffeechatAcceptConfirm';
 import CoffeechatRejectConfirm from '../CoffeechatRejectConfirm';
+import { type CoffeechatState, CoffeechatStateEnum } from '@/types/coffeechat';
 import type { User } from '@/types/user';
 
 type CoffeechatReceiveCardProps = {
   fromUser: User;
   coffeechatId: number;
+  state: CoffeechatState;
 };
 
 const CoffeechatReceiveCard = ({
   fromUser,
   coffeechatId,
+  state,
 }: CoffeechatReceiveCardProps) => {
   const overlay = useOverlay();
-  const userType = fromUser.isGraduated ? '졸업생' : '재학생';
+  const isPending = state === 'PENDING';
 
   const openCoffeechatAcceptConfirm = () => {
     overlay.open(({ isOpen, close }) => (
@@ -39,27 +43,41 @@ const CoffeechatReceiveCard = ({
 
   return (
     <StyledCoffeechatReceiveCard direction="vertical" spacing={24}>
-      <Stack direction="vertical" spacing={6}>
-        <Text fontType="body1">
-          {fromUser.cardinal}기 {userType} {fromUser.name}님이 커피챗 신청을 보냈어요!
-        </Text>
-        <Text fontType="body2" color="gray600">
-          커피챗 신청을 수락하시겠습니까?
-        </Text>
-      </Stack>
-      <Stack direction="horizontal" align="center" spacing={12}>
-        <Button
-          onClick={openCoffeechatRejectConfirm}
-          styleType="secondary"
-          size="medium"
-          width="120px"
-        >
-          거절하기
-        </Button>
-        <Button onClick={openCoffeechatAcceptConfirm} size="medium">
-          수락하기
-        </Button>
-      </Stack>
+      {isPending ? (
+        <Stack direction="vertical" spacing={6}>
+          <Text fontType="body1">{fromUser.name}님이 커피챗 신청을 보냈어요!</Text>
+          <Text fontType="body2" color="gray600">
+            {fromUser.cardinal}기 {fromUser.isGraduated ? '졸업생' : '재학생'}
+          </Text>
+        </Stack>
+      ) : (
+        <Flex justify="space-between" align="center">
+          <Stack direction="vertical" spacing={6}>
+            <Text fontType="body1">{fromUser.name}님이 커피챗 신청을 보냈었어요.</Text>
+            <Text fontType="body2" color="gray600">
+              {fromUser.cardinal}기 {fromUser.isGraduated ? '졸업생' : '재학생'}
+            </Text>
+          </Stack>
+          <StyledCoffechatStatus state={state}>
+            {CoffeechatStateEnum[state]} 완료
+          </StyledCoffechatStatus>
+        </Flex>
+      )}
+      {isPending && (
+        <Stack direction="horizontal" align="center" spacing={12}>
+          <Button
+            onClick={openCoffeechatRejectConfirm}
+            styleType="secondary"
+            size="medium"
+            width="120px"
+          >
+            거절하기
+          </Button>
+          <Button onClick={openCoffeechatAcceptConfirm} size="medium">
+            수락하기
+          </Button>
+        </Stack>
+      )}
     </StyledCoffeechatReceiveCard>
   );
 };
@@ -68,7 +86,14 @@ export default CoffeechatReceiveCard;
 
 const StyledCoffeechatReceiveCard = styled(Stack)`
   width: 100%;
-  height: 150px;
   margin-bottom: 24px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray200};
+  padding-bottom: 16px;
+`;
+
+const StyledCoffechatStatus = styled.div<{ state: CoffeechatState }>`
+  ${({ theme, state }) => css`
+    ${theme.fonts.body2}
+    color: ${state === 'REJECT' ? theme.colors.red : theme.colors.primary};
+  `}
 `;
