e4d9a8c02e9b538effd4dfe6eccf2dcd56a5c4da
김석진 <sj01048260279@gmail.com>
2024-04-02 11:05:11 +0900

    fix(client): qna write constants 경로 변경

--- apps/client/src/constants/qna.ts
+++ apps/client/src/constants/qna.ts
@@ -130,3 +130,18 @@ export const QNA_COMMENT = [
     name: 'kth0303',
   },
 ];
+
+export const QNA_WRITE_CATEGORY = [
+  {
+    id: 0,
+    qnaType: 'DEVELOP',
+  },
+  {
+    id: 1,
+    qnaType: 'RECRUIT',
+  },
+  {
+    id: 2,
+    qnaType: 'CONCERN',
+  },
+];
