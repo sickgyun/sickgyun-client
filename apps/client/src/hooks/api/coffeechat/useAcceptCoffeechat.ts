0ecf5af0896e061b75e75299e5c311b347f8a98a
김석진 <sj01048260279@gmail.com>
2024-04-04 00:21:56 +0900

    feat(client): 커피챗 수락, 거절을 하였을 경우 유저 데이터 업데이트

--- apps/client/src/hooks/api/coffeechat/useAcceptCoffeechat.ts
+++ apps/client/src/hooks/api/coffeechat/useAcceptCoffeechat.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { RECEIVE_COFFEE_CHAT_LIST } from './useGetReceiveCoffeechatList';
 import { put } from '@/libs/api/client';
+import { USER_QUERY_KEY } from '../user/useGetUser';
 
 type UseAcceptCoffeechatProps = {
   coffeechatId: number;
@@ -17,6 +18,7 @@ export const useAcceptCoffeechat = ({
     mutationFn: () => put(`/api/coffeechat/${coffeechatId}/accept`),
     onSuccess: (response: any) => {
       queryClient.invalidateQueries({ queryKey: [RECEIVE_COFFEE_CHAT_LIST] });
+      queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY] });
       openCoffeechatMessageModal(response?.message);
     },
   });
--- apps/client/src/hooks/api/coffeechat/useRejectCoffeechat.ts
+++ apps/client/src/hooks/api/coffeechat/useRejectCoffeechat.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { USER_QUERY_KEY } from '../user/useGetUser';
 import { RECEIVE_COFFEE_CHAT_LIST } from './useGetReceiveCoffeechatList';
 import { put } from '@/libs/api/client';
 
@@ -14,6 +15,7 @@ export const useRejectCoffeechat = (coffeechatId: number) => {
       put(`/api/coffeechat/${coffeechatId}/reject`, data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [RECEIVE_COFFEE_CHAT_LIST] });
+      queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY] });
       alert('커피챗 응답이 거절되었어요.');
     },
   });
