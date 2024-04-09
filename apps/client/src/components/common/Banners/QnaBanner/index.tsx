147a8147f2e6d1bdbed3ad5729a5d44c27934ac4
김석진 <sj01048260279@gmail.com>
2024-04-09 11:33:42 +0900

    feat(client): 리디자인된 qna 배너 개발

---
+++ apps/client/src/components/common/Banners/QnaBanner/index.tsx
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+import { Stack, Text } from '@sickgyun/ui';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { isProd } from '@/utils/isProd';
+
+const QnaBanner = () => {
+  const router = useRouter();
+
+  const handleGoQnaPage = () => {
+    if (isProd(process.env.NODE_ENV)) {
+      alert('상진이가 열심히 개발중이에요!');
+      return;
+    }
+    router.push(`/qna`);
+  };
+
+  return (
+    <StyledQnaBanner onClick={handleGoQnaPage}>
+      <Stack direction="vertical" spacing={8}>
+        <Text fontType="h2" color="gray900">
+          개발부터 커리어까지
+        </Text>
+        <Text fontType="h1" color="gray900">
+          부산소마고에 관련한 모든 것, Q&A에서 물어보세요!
+        </Text>
+      </Stack>
+      <Image src="/assets/svgs/logo.svg" alt="Symbol" width={140} height={48} />
+    </StyledQnaBanner>
+  );
+};
+
+export default QnaBanner;
+
+const StyledQnaBanner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  padding: 50px;
+  background-image: url('/assets/images/qna_banner_background.png');
+  object-fit: cover;
+  border-radius: 8px;
+  width: 100%;
+  height: 150px;
+  border-radius: 16px;
+  cursor: pointer;
+`;
