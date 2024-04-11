1be0f20c4cbbc79ae8d6d5e535f210de622a7fed
김석진 <sj01048260279@gmail.com>
2024-04-12 08:16:27 +0900

    chore(client): 프로필 추가 유도 버튼 클릭 이벤트가 동작하지 않는 오류 수정

--- apps/client/src/components/coffeechat/CoffeechatReceiveList/index.tsx
+++ apps/client/src/components/coffeechat/CoffeechatReceiveList/index.tsx
@@ -1,13 +1,19 @@
 import styled from '@emotion/styled';
 import { Button, Spinner, Stack, Text } from '@sickgyun/ui';
 import { isEmpty } from 'lodash';
+import { useRouter } from 'next/navigation';
 import CoffeechatReceiveCard from '../CoffeechatReceiveCard';
 import { useGetReceiveCoffeechatList } from '@/hooks/api/coffeechat/useGetReceiveCoffeechatList';
 import { useUser } from '@/hooks/common/useUser';
 
 const CoffeechatReceiveList = () => {
+  const router = useRouter();
   const { receiveCoffeechatList, isLoading } = useGetReceiveCoffeechatList();
   const { user } = useUser();
+
+  const handleGoProfileCreatePage = () => {
+    router.push('/profile/create');
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -29,10 +35,12 @@ const CoffeechatReceiveList = () => {
         <Text fontType="h3">앗! 요청 받은 내역이 없어요...</Text>
       ) : (
         <Stack direction="vertical" align="center" justify="center" spacing={16}>
-          <Button size="medium" width="200px">
+          <Button onClick={handleGoProfileCreatePage} size="medium" width="200px">
             프로필 등록하러 가기
           </Button>
-          <Text fontType="body1">프로필 등록을 하면 커피챗 요청을 받을 수 있어요!</Text>
+          <Text fontType="p1" color="gray900">
+            프로필 등록을 하면 커피챗 요청을 받을 수 있어요!
+          </Text>
         </Stack>
       )}
     </StyledCoffeechatReceiveList>
