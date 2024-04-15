f3079e0f8eaaf127c0a9119c3fd557d860cbce47
김석진 <sj01048260279@gmail.com>
2024-04-15 18:20:59 +0900

    fix(client): 구글 애널리틱스 측정 아이디 환경 변수명 변경

--- apps/client/src/components/common/GoogleAnalytics/index.tsx
+++ apps/client/src/components/common/GoogleAnalytics/index.tsx
@@ -4,14 +4,14 @@ export const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
         `}
       </Script>
     </>
