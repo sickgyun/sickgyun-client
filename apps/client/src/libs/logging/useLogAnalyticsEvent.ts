8505aa017b8d6cd091e9933f612c6e62b0d61183
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-04-22 00:08:36 +0900

    feat(client): 로깅 default params에 date, time 추가 (#319)

--- apps/client/src/libs/logging/useLogAnalyticsEvent.ts
+++ apps/client/src/libs/logging/useLogAnalyticsEvent.ts
@@ -1,6 +1,8 @@
 import { useMemo } from 'react';
 import { useUser } from '@/hooks/common/useUser';
 import { type LogAnalyticsEvent, logAnalyticsEvent } from '@/libs/logging';
+import { getDate } from '@/utils/getDate';
+import { getTime } from '@/utils/getTime';
 
 export const useLogAnalyticsEvent = () => {
   const { user } = useUser();
@@ -8,19 +10,29 @@ export const useLogAnalyticsEvent = () => {
   const { logShowEvent, logClickEvent } = useMemo(() => {
     return {
       logShowEvent(event: LogAnalyticsEvent) {
+        const date = getDate();
+        const time = getTime();
+
         logAnalyticsEvent({
           ...event,
           params: {
             userId: user.id,
+            date,
+            time,
             ...event?.params,
           },
         });
       },
       logClickEvent(event: LogAnalyticsEvent) {
+        const date = getDate();
+        const time = getTime();
+
         logAnalyticsEvent({
           ...event,
           params: {
             userId: user.id,
+            date,
+            time,
             ...event?.params,
           },
         });
