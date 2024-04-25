8063b65a0531f46bc32ea983e4e674fa0e3dc362
김석진 <sj01048260279@gmail.com>
2024-04-25 23:12:15 +0900

    chore(storybook, ui): 불필요한 라이브러리 삭제 & gitignore에 storybook-static 추가

--- docs/storybook/.storybook/main.ts
+++ docs/storybook/.storybook/main.ts
@@ -6,9 +6,10 @@ import { join, dirname } from 'path';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+const getAbsolutePath = (value: string): any => {
   return dirname(require.resolve(join(value, 'package.json')));
-}
+};
+
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
