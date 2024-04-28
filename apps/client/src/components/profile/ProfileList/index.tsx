bd3f249bb38bfe05d6f0b4eb17406fb7c438267a
김석진 <sj01048260279@gmail.com>
2024-04-28 22:52:55 +0900

    chore(client): 프로필 리스트 없을 시 나오는 텍스트 폰트 토큰 변경

--- apps/client/src/components/profile/ProfileList/index.tsx
+++ apps/client/src/components/profile/ProfileList/index.tsx
@@ -44,7 +44,7 @@ const ProfileList = ({ major, isRecruited, cardinal, promotion }: ProfileListPro
       ))}
     </StyledProfileList>
   ) : (
-    <Text fontType="h3">앗! 해당 분야의 학생이 없어요..</Text>
+    <Text fontType="h4">앗! 해당 분야의 학생이 없어요..</Text>
   );
 };
 
