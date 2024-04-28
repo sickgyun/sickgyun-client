0bffa422a3b9cc2c62b9788832e41add277feadc
김석진 <sj01048260279@gmail.com>
2024-04-29 08:52:11 +0900

    feat(client): 부마위키 프로필 리스트에 권세원 추가

--- apps/client/src/constants/profile.ts
+++ apps/client/src/constants/profile.ts
@@ -33,6 +33,6 @@ export const DIRECT_PROFILE_LIST = [
   },
 ] as const;
 
-export const BUMAWIKI_PROFILE_ID_LIST = [5, 13, 7, 14, 16];
+export const BUMAWIKI_PROFILE_ID_LIST = [5, 13, 7, 14, 16, 45];
 
 export const SICKGYUN_PROFILE_ID_LIST = [5, 15, 3, 4];
